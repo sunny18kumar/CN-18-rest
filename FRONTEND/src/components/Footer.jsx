f47f46f96a2c388783dcb2cb8d17f6394f25176f
sunny kumar <133806140+sunny18kumar@users.noreply.github.com>
2024-02-13 00:10:22 +0530

    Add files via upload

---
+++ FRONTEND/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">CN-18</div>
+          <div className="right">
+            <p>Patna , Bihar India</p>
+            <p>Open: 08:00 AM - 10:00 PM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Sunny</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Sunny.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
